3ea476cf2439d38bf9227039972d212ceb431b65
elvisuriel <urielmarciales@gmil.com>
2024-06-12 18:38:54 -0500

    ajustando server para usuarios en linea

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import http from 'http';
 import cors from 'cors';
 import { PORT } from './config.js';
 
-
 const app = express();
 const server = http.createServer(app);
 
@@ -15,15 +14,28 @@ const io = new SocketServer(server, {
   }
 });
 
-
 app.use(cors());
 app.use(morgan('dev'));
 
+let onlineUsers = [];
 
 io.on('connection', (socket) => {
+  console.log('New user connected:', socket.id);
+
+  socket.on('userConnected', (username) => {
+    onlineUsers.push({ id: socket.id, username });
+    io.emit('users', onlineUsers.map(user => user.username));
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+    onlineUsers = onlineUsers.filter(user => user.id !== socket.id);
+    io.emit('users', onlineUsers.map(user => user.username));
+  });
+
   socket.on('message', (msg) => {
     socket.broadcast.emit('message', { body: msg.body, user: msg.user });
-  })
+  });
 });
 
 server.listen(PORT);
